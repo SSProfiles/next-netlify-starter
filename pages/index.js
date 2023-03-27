16630f1df7df77434f5d13dc03a66352fd7694ae
SSProfiles <128578645+SSProfiles@users.noreply.github.com>
2023-03-27 13:03:23 +0530

    added component to the home file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Home from '@components/Home'
 
 export default function Home() {
   return (
@@ -11,7 +12,8 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+       <Home/>
+        <Header title="Welcome to my application" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
